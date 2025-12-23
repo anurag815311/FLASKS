c463336223af381617474a382cf50bf5b30e7201
Anurag Singh <anuragsingh815311@gmail.com>
2025-12-23 19:55:42 +0530

    Initial commit: Flask blog with authentication and CRUD

---
+++ app.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+# ---------------- MODELS ----------------
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(200))
+    posts = db.relationship('Post', backref='author')
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+# -------------- LOGIN LOADER --------------
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# ---------------- ROUTES ----------------
+@app.route('/')
+@login_required
+def dashboard():
+    posts = Post.query.filter_by(user_id=current_user.id)
+    return render_template('dashboard.html', posts=posts)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        hashed_pw = generate_password_hash(request.form['password'])
+        user = User(username=request.form['username'], password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect('/')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
+
+@app.route('/create', methods=['GET','POST'])
+@login_required
+def create():
+    if request.method == 'POST':
+        post = Post(
+            title=request.form['title'],
+            content=request.form['content'],
+            user_id=current_user.id
+        )
+        db.session.add(post)
+        db.session.commit()
+        return redirect('/')
+    return render_template('create_post.html')
+@app.route('/edit/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def edit(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    # Security check
+    if post.user_id != current_user.id:
+        return redirect('/')
+
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/')
+
+    return render_template('edit_post.html', post=post)
+
+
+@app.route('/delete/<int:post_id>')
+@login_required
+def delete(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    # Security check
+    if post.user_id != current_user.id:
+        return redirect('/')
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
